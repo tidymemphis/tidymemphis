a18a2bb0c281045c0f95be0b47ea498eb7758efc
Sarah Castle <sarahviamemphis@gmail.com>
2020-01-31 15:06:36 -0600

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+        <img style={{ margin: 0 }} src="./bts-0.jpg" alt="Gatsby Scene" />
         <h1>
           not yet not yet{" "}
           <span role="img" aria-label="wave emoji">
