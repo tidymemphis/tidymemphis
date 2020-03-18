671ea619bd840d1445d6ebfaa85c1ae6935f620e
Sarah Castle <sarahviamemphis@gmail.com>
2020-03-17 20:55:04 -0500

    20200317-04

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,11 +18,19 @@ class IndexPage extends React.Component {
 
         Send money directly to employees at your favorite restaurants:
         <p><a href={`https://docs.google.com/spreadsheets/d/1ToAhSOm0SN7u0blufJVNU8i-Tf24zGz5LHg8ClfnCk8/edit?usp=sharing`}>
-          Memphis Virtual Tip Jar
+          <b>Memphis Virtual Tip Jar</b>
         </a></p>
         Service industry employees: <p><a href={`https://docs.google.com/forms/d/e/1FAIpQLSd50axYGcCPOn5NRE_gw52n36hgJDLw5vhDCDDOClAILkB_eg/viewform?usp=sf_link`}>
-          fill out this form to be added to the tip jar list
+          fill out this form to be added to the tip jar
         </a></p>
+
+        <p><a href={`https://docs.google.com/spreadsheets/d/16g95WUiwiKZtd2a4fvR268E655Sa0n5qY0hMu0Y8Mhw/edit?usp=sharing`}>
+          Click here </a>for a list of restaurant changes due to coronavirus.</p>
+
+        <p>Enter the status of a restaurant <a href={`https://docs.google.com/forms/d/e/1FAIpQLSfrBhGiTU9czsPqLhF-UgsBOuPPIfL_DkRtgPexAaPFGD6jgQ/viewform?usp=sf_link`}>here</a></p>
+
+
+
         <p>
         <b>Other ways to help:</b>
         </p>
